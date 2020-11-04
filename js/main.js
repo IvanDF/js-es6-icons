3b2f1fb901e852c28d42d4f9d8f400143c183ad8
Ivan <delfatti.ivan@gmail.com>
2020-11-04 14:40:23 +0100

    Print icons

---
+++ js/main.js
@@ -0,0 +1,135 @@
+/*********************
+ * Cats filter
+*********************/
+
+$(document).ready(function(){
+
+    // Icon database
+    const icons = [
+        {
+          name: 'cat',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'crow',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'dog',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'dove',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'dragon',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'horse',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'hippo',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'fish',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'carrot',
+          prefix: 'fa-',
+          type: 'vegetable',
+          family: 'fas',
+        },
+        {
+          name: 'apple-alt',
+          prefix: 'fa-',
+          type: 'vegetable',
+          family: 'fas',
+        },
+        {
+          name: 'lemon',
+          prefix: 'fa-',
+          type: 'vegetable',
+          family: 'fas',
+        },
+        {
+          name: 'pepper-hot',
+          prefix: 'fa-',
+          type: 'vegetable',
+          family: 'fas',
+        },
+        {
+          name: 'user-astronaut',
+          prefix: 'fa-',
+          type: 'user',
+          family: 'fas',
+        },
+        {
+          name: 'user-graduate',
+          prefix: 'fa-',
+          type: 'user',
+          family: 'fas',
+        },
+        {
+          name: 'user-ninja',
+          prefix: 'fa-',
+          type: 'user',
+          family: 'fas',
+        },
+        {
+          name: 'user-secret',
+          prefix: 'fa-',
+          type: 'user',
+          family: 'fas',
+        },
+    ];
+
+    // ICONS SECTION
+    const inconsSection = $('.icons-section');
+
+    printIcons(icons, inconsSection);
+
+
+}); // <-- end doc. ready
+
+// PRINT ICONS FUNCTION
+function printIcons( icons, inconsSection ) {
+
+    icons.forEach( ( i ) => {
+        // DESTRUCTURING
+        const { family, prefix, name } = i
+
+        // PRINT INTO DOM
+        const printIcon = 
+        `
+        <div class="icon-container txt-upper">
+            <i class="${family} ${prefix}${name}"></i>
+            <p class="icon-name">${name}</p>
+        </div>
+        ` 
+
+        return inconsSection.append(printIcon)
+    });
+
+
+};
